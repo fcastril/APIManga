97af6b924c176910e254ca119bc8eeb3aba193d4
Juan Pablo Castrillón <jpcastrillonr@gmail.com>
2026-04-13 16:08:37 -0500

    API biblioteca mangas

---
+++ Controllers/MangaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BibliotecaMangasAPI.models;
+
+namespace BibliotecaMangasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MangaController : ControllerBase
+    {
+        private static List<Manga> biblioteca = new List<Manga>();
+
+        private static int contadorId = 1;
+
+        [HttpPost]
+        public ActionResult<Manga> CrearManga(Manga manga)
+        {
+            if (string.IsNullOrWhiteSpace(manga.titulo))
+            {
+                return BadRequest("El título no puede estar vacío");
+            }
+            
+            manga.Id = contadorId;
+            contadorId++;
+
+            biblioteca.Add(manga);
+
+            return Ok(manga);
+        }
+    }
+}
---
+++ models/Manga.cs
@@ -0,0 +1,11 @@
+﻿namespace BibliotecaMangasAPI.models
+{
+    public class Manga
+    {
+        public int  Id {  get; set; }
+        public string titulo { get; set; }
+        public string autor {  get; set; }
+        public int NumeroTomo { get; set; }
+        public double precio { get; set; }
+    }
+}
